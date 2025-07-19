166c654fad1d5edfe4682c3a805e0839c79501e4
namgyumin <40926053+namgyumin@users.noreply.github.com>
2025-07-19 17:21:20 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -7,29 +7,26 @@ using namespace std;
 
 struct coor{
     int i, j;
+
     coor(int a, int b) : i(a) , j(b) {}
 };
 
-int ans;
+int ans, bigSize, n;
 
 void rescurive(coor start, int size, int &number, coor goal);
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, i, j;
-
+    int i, j;
+    
     cin >> n >> i >> j;
 
-    int initial_size = static_cast<int>(pow(2, n));
+    bigSize = pow(2,n);
     int number = 0;
 
-    rescurive(coor(0,0), initial_size, number, coor(i,j));
+    rescurive(coor(0,0), pow(2,n), number, coor(i,j));
 
-    cout << ans << endl;
+    cout << ans;
 
-    return 0;
 }
 
 void rescurive(coor start, int size, int &number, coor goal){
@@ -38,25 +35,27 @@ void rescurive(coor start, int size, int &number, coor goal){
             ans = number;
             return;
         }
-        return;
+        number++;
     }
 
-    int temp = size/2;
-    int quad_area = temp * temp;
+    else{
+        int temp = size/2;
 
-    if((goal.i >= start.i && goal.i < start.i + temp) && (goal.j >= start.j && goal.j < start.j + temp)){
-        rescurive(coor(start.i, start.j), temp, number, goal);
-    }
-    else if ((goal.i >= start.i && goal.i < start.i + temp) && (goal.j >= start.j + temp)){
-        number += quad_area;
-        rescurive(coor(start.i, start.j + temp), temp, number, goal);
-    }
-    else if ((goal.i >= start.i + temp) && (goal.j >= start.j && goal.j < start.j + temp)){
-        number += 2 * quad_area;
-        rescurive(coor(start.i + temp, start.j), temp, number, goal);
-    }
-    else {
-        number += 3 * quad_area;
-        rescurive(coor(start.i + temp, start.j + temp), temp, number, goal);
+        if((goal.i >= start.i && goal.i < start.i + temp) && (goal.j >= start.j && goal.j < start.j + temp)){
+            rescurive(coor(start.i, start.j), temp, number, goal);
+        }
+        else if ((goal.i >= start.i && goal.i < start.i + temp) && (goal.j >= start.j + temp)){
+            number += temp * temp;
+            rescurive(coor(start.i, start.j+temp), temp, number, goal);
+        }
+        else if ((goal.i >= start.i + temp) && (goal.j >= start.j && goal.j < start.j + temp)){
+            number += temp * temp * 2;
+            rescurive(coor(start.i+temp, start.j), temp, number, goal);
+        }
+        else {
+            number += temp * temp * 3;
+            rescurive(coor(start.i+temp, start.j+temp), temp, number, goal);
+        }
+        
     }
 }
