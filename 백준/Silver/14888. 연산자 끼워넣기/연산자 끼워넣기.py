f1762d99c51e1d5f74e17a61df2fddd49acb2878
namgyumin <40926053+namgyumin@users.noreply.github.com>
2026-03-03 23:09:56 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 164 ms, Memory: 112140 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -2,33 +2,37 @@ import sys
 
 n = int(sys.stdin.readline())
 
-numArr =  list(map(int, sys.stdin.readline().split()))
+numArr = list(map(int, sys.stdin.readline().split()))
 cal = list(map(int, sys.stdin.readline().split()))
 
-def function(plus, minus, mutiply, divide, numArr, cal, step, value, maxim, minum, n):
-    if step == 0:
-        return function(plus, minus, mutiply, divide, numArr, cal, step+1, numArr[0], maxim, minum, n)
-    if n == step:
-        return value, value
-    else:
-        if cal[0] > plus:
-            ma, mi = function(plus+1, minus, mutiply, divide, numArr, cal, step+1, value+numArr[step], maxim, minum, n)
-            maxim = max(maxim, ma)
-            minum = min(minum, mi)
-        if cal[1] > minus:
-            ma, mi = function(plus, minus+1, mutiply, divide, numArr, cal, step+1, value-numArr[step], maxim, minum, n)
-            maxim = max(maxim, ma)
-            minum = min(minum, mi)
-        if cal[2] > mutiply:
-            ma, mi = function(plus, minus, mutiply+1, divide, numArr, cal, step+1, value*numArr[step], maxim, minum, n)
-            maxim = max(maxim, ma)
-            minum = min(minum, mi)
-        if cal[3] > divide:
-            ma, mi = function(plus, minus, mutiply, divide+1, numArr, cal, step+1, int(value/numArr[step]), maxim, minum, n)
-            maxim = max(maxim, ma)
-            minum = min(minum, mi)
-        return maxim, minum
+def applyToCalculator(firstValue, secondValue, idxValue):
+    if idxValue == 0:
+        return firstValue + secondValue
+    if idxValue == 1:
+        return firstValue - secondValue
+    if idxValue == 2:
+        return firstValue * secondValue
+    if idxValue == 3:
+        return int(firstValue / secondValue)
 
-ma, mi = function(0, 0, 0, 0, numArr, cal, 0, 0, -(10**18), 10**18, n)
-print(ma)
+mx = -10**18
+mi = 10**18
+
+def dfs(step, value):
+    global mx
+    global mi
+    if step == n:
+        mx = max(mx, value)
+        mi = min(mi, value)
+        return
+    for i in range(4):
+        if cal[i] == 0:
+            continue
+        cal[i] -= 1
+        dfs(step+1, applyToCalculator(value, numArr[step], i))
+        cal[i] += 1
+
+dfs(1, numArr[0])
+
+print(mx)
 print(mi)
